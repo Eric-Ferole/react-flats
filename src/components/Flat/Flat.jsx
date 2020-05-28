71007e3594a5431db849492bd1bcd1f4a85d27b1
Eric Ferole <eferole@pixelcircus.ca>
2020-05-27 21:03:04 -0400

    Flats almost done

--- src/components/Flat/Flat.jsx
+++ src/components/Flat/Flat.jsx
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
+// eslint-disable-next-line react/prefer-stateless-function
 class Flat extends Component {
-
-render() {
-
-        return (
-            <div className="flat">
-                <p className="flat__price">{this.props.price} {this.props.priceCurrency}</p>
-                <p className="flat__name">{this.props.name}</p>
-                <a href="/"><img src={this.props.imageUrl} alt=""/></a>
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div className="flat">
+        <p className="flat__price">
+          {this.props.price} {this.props.priceCurrency}
+        </p>
+        <p className="flat__name">{this.props.name}</p>
+        <a href="/">
+          <img src={this.props.imageUrl} alt="" />
+        </a>
+      </div>
+    );
+  }
 }
 
 export default Flat;
--- src/components/MyGoogleMap/MyGoogleMap.jsx
+++ src/components/MyGoogleMap/MyGoogleMap.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 
 // API KEY
-import API_KEY from "../../../../../API/googleMapAPIKEY";
+import CLE from "../../../../../API/googleMapAPIKEY";
+
 
 // Components
 import Marker from "../Marker/Marker";
@@ -17,22 +18,22 @@ class SimpleMap extends Component {
       lat: 48.8534,
       lng: 2.3488,
     },
-    zoom: 11,
+    zoom: 13,
   };
 
   render() {
-
+    console.log("FUCK FUCK", CLE)
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: "100vh", width: "100%" }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ API_KEY }}
+          bootstrapURLKeys={ { key: CLE } }
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
           {
-            this.props.flats.map(flat => {
-              return <Marker lat={flat.lat} lng={flat.lng} id={flat.id} text="My Marker" />
+            this.props.flats.map((flat) => {
+              return <Marker lat={flat.lat} lng={flat.lng} key={flat.id} text="My Marker" />
             })
           }
         </GoogleMapReact>
